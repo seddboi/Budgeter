6b9e56dfe0ab3b0a5913f154cd33c818864d2df9
Gian Zamora <gz2016@att.net>
2021-06-17 09:56:39 -0700

    added update database javascript page

---
+++ public/database.js
@@ -0,0 +1,58 @@
+const database;
+
+const budgetRequest = indexedDB.open('budget', 1);
+
+budgetRequest.onerror = () => {
+    console.log('Please try again!')
+};
+
+budgetRequest.onupgradeneeded = (event) => {
+    database = event.target.result;
+
+    database.createObjectStore('pending', {autoIncrement:true});
+};
+
+budgetRequest.onsuccess = (event) => {
+    database = event.target.result;
+
+    if (navigator.onLine) {
+        getData();
+    }
+};
+
+function saveData(data) {
+    const transaction = database.transaction(['pending'], 'readwrite');
+
+    const storeTransaction = transaction.objectStore('pending');
+
+    storeTransaction.add(data);
+};
+
+function getData() {
+    const transaction = database.transaction(['pending'], 'readwrite');
+
+    const storeTransaction = transaction.objectStore('pending');
+
+    const gatherData = storeTransaction.getAll();
+
+    gatherData.onsuccess = () => {
+        if (gatherData.result.length > 0) {
+            fetch("/api/transactions/bulk", {
+                method:'POST', 
+                body: JSON.stringify(gatherData.result),
+                headers: {
+                    Accept: 'application/json , text/plain', 'Content-Type': 'application/json' 
+                },
+            }).then( (response) => {
+                response.json();
+
+                const transaction = database.transaction(['pending'], 'readwrite');
+
+                const storeTransaction = transaction.objectStore('pending');
+
+                storeTransaction.clear();
+            });
+        }
+    }
+};
+
